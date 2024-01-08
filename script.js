8b494265814c8c26f3226f2e4a271fc8c680d65a
melcholign <115886450+melcholign@users.noreply.github.com>
2024-01-08 18:44:18 +0600

    Feature a user-input based grid maker and drawing capabilities
    The initial grid of 50x50 squares can be modified by prompting the user
    upon the push of a button to the enter the number of squares per side,
    which instantly generates a new grid. Note that the maximum limit is set
    to 100 squares due to performance and rendering issues.
    Next, each square is highlighted by mouse hover, and permanently
    filled in with a dark color if the mouse moves over it, between pressing
    down and releasing it.

--- script.js
+++ script.js
@@ -1,16 +1,120 @@
 "use strict"
 
-const body = document.querySelector('body');
+let drawing = false;
 
-const gridContainer = makeGridContainer(480, 480);
-body.appendChild(gridContainer);
+initializeBody(makeGridInputButton, makeGridContainer);
+
+function initializeBody(makeGridInputButton, makeGridContainer) {
+    const body = document.querySelector('body');
+    body.className = 'column-center';
+    const gridContainer = makeGridContainer(540, 540);
+    fillWithGridSquares(gridContainer, 50);
+    const gridInputButton = makeGridInputButton(gridContainer);
+    body.appendChild(gridInputButton);
+    body.appendChild(gridContainer);
+
+    return body;
+}
+
+function removeGrids(gridContainer) {
+
+    const gridSquares = gridContainer.childNodes;
+    while (gridSquares.length) {
+        gridContainer.removeChild(gridSquares[0]);
+    }
+}
+
+function getGridInput() {
+
+    while (true) {
+
+        let gridInput = prompt("Enter the no. of squares (max: 100) per side of the grid: ");
+        if (gridInput === null) return null;
+
+        gridInput = +gridInput;
+
+        if (isNaN(gridInput) || !Number.isInteger(gridInput) || gridInput < 0) {
+            alert('Invalid input');
+        } else if (gridInput > 100) {
+            alert('Accepted input value cannot be greater than 100');
+        } else {
+            return gridInput;
+        }
+    }
+}
+
+function makeGridInputButton(gridContainer) {
+    const inputButton = document.createElement('button');
+
+    inputButton.id = 'input-button';
+    inputButton.textContent = 'Change No. of Squares Per Side';
+
+    inputButton.addEventListener('click', () => {
+        const gridInput = getGridInput();
+        if (gridInput !== null) {
+            removeGrids(gridContainer);
+            fillWithGridSquares(gridContainer, gridInput);
+        }
+    });
+
+    return inputButton;
+}
+
+function addDrawingEffect(gridSquare) {
+
+    let filled = false;
+
+    gridSquare.addEventListener('mousedown', () => {
+        drawing = true;
+        filled = true;
+    });
+
+    gridSquare.addEventListener('mouseup', () => drawing = false);
+    gridSquare.addEventListener('dragstart', () => drawing = false);
+
+    gridSquare.addEventListener('mouseenter', () => {
+        if (drawing) filled = true;
+        gridSquare.style.backgroundColor = '#acacac'
+    });
+
+    gridSquare.addEventListener('mouseleave', () => {
+        if (!filled) gridSquare.style.backgroundColor = '';
+    });
+}
+
+function fillWithGridSquares(gridContainer, gridsPerSide) {
+
+    const gridSize = parseFloat(gridContainer.style.width) / gridsPerSide;
+
+    for (let i = 1; i <= gridsPerSide; ++i) {
+
+        for (let j = 1; j <= gridsPerSide; ++j) {
+
+            const gridSquare = makeGridSquare(gridSize);
+            addDrawingEffect(gridSquare);
+            gridContainer.appendChild(gridSquare);
+        }
+    }
+}
+
+function makeGridSquare(gridSize) {
+    const GridSquare = document.createElement('div');
+
+    GridSquare.className = 'grid-square';
+    GridSquare.style.width = `${gridSize}px`;
+    GridSquare.style.height = `${gridSize}px`;
+
+    return GridSquare;
+}
 
 function makeGridContainer(width, height) {
     const gridContainer = document.createElement('div');
 
     gridContainer.id = 'grid-container';
+    gridContainer.className = 'column-center';
     gridContainer.style.width = `${width}px`;
     gridContainer.style.height = `${height}px`;
 
     return gridContainer;
 }
+
